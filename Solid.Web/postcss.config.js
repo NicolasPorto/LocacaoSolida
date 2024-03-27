63f6b5727474cb96478b39630114cf2b87b4413e
Nicolas Porto <111699513+NicolasPorto@users.noreply.github.com>
2024-03-26 21:42:03 -0300

    feat

--- Solid.Web/postcss.config.js
+++ Solid.Web/postcss.config.js
@@ -1,20 +1,19 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+    ],
+    whitelistPatterns: [/^slick-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    process.env.NODE_ENV === "production" &&
-      purgecss({
-        content: [
-          "./pages/**/*.js",
-          "./pages/**/*.jsx", // Adicionado para incluir arquivos JSX
-          "./components/**/*.js",
-          "./components/**/*.jsx", // Adicionado para incluir arquivos JSX
-        ],
-        whitelistPatterns: [/^slick-/],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      }),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
--- Solid.Web/tailwind.config.js
+++ Solid.Web/tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   content: [
-    "./pages/*.jsx",
-    "./pages/**/*.jsx",
-    "./components/*.jsx",
-    "./components/**/*.jsx",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     boxShadow: {
