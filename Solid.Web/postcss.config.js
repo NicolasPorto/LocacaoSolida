f76c4eb4eb2d074b8223de94eb421509b584be18
Nicolas Porto <111699513+NicolasPorto@users.noreply.github.com>
2024-03-26 21:35:54 -0300

    feat

--- Solid.Web/postcss.config.js
+++ Solid.Web/postcss.config.js
@@ -1,21 +1,20 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: [
-      "./pages/*.js",
-      "./pages/**/*.js",
-      "./components/*.js",
-      "./components/**/*.js",
-    ],
-    whitelistPatterns: [/^slick-/],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
     "autoprefixer",
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    process.env.NODE_ENV === "production" &&
+      purgecss({
+        content: [
+          "./pages/**/*.js",
+          "./pages/**/*.jsx", // Adicionado para incluir arquivos JSX
+          "./components/**/*.js",
+          "./components/**/*.jsx", // Adicionado para incluir arquivos JSX
+        ],
+        whitelistPatterns: [/^slick-/],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      }),
   ],
 };
